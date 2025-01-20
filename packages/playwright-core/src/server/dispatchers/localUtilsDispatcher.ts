b38e6783c17e81aa28af7d232d9f18de2bd7f366
Simon Knott <info@simonknott.de>
2025-01-20 14:35:22 +0100

    rip apart

--- packages/playwright-core/src/server/dispatchers/localUtilsDispatcher.ts
+++ packages/playwright-core/src/server/dispatchers/localUtilsDispatcher.ts
@@ -20,13 +20,13 @@ import path from 'path';
 import os from 'os';
 import type * as channels from '@protocol/channels';
 import { ManualPromise } from '../../utils/manualPromise';
-import { assert, calculateSha1, createGuid, HttpServer, monotonicTime, removeFolders, urlMatches } from '../../utils';
+import { assert, calculateSha1, createGuid, HttpServer, removeFolders, urlMatches } from '../../utils';
 import type { RootDispatcher } from './dispatcher';
 import { Dispatcher } from './dispatcher';
 import { yazl, yauzl } from '../../zipBundle';
 import { ZipFile } from '../../utils/zipFile';
 import type * as har from '@trace/har';
-import type { HeadersArray, NormalizedContinueOverrides, NormalizedFulfillResponse } from '../types';
+import type { HeadersArray } from '../types';
 import { JsonPipeDispatcher } from '../dispatchers/jsonPipeDispatcher';
 import { WebSocketTransport } from '../transport';
 import { SocksInterceptor } from '../socksInterceptor';
@@ -36,22 +36,15 @@ import type { Progress } from '../progress';
 import { ProgressController } from '../progress';
 import { fetchData } from '../../utils/network';
 import type { HTTPRequestParams } from '../../utils/network';
-import http from 'http';
-import https from 'https';
+import type http from 'http';
 import type { Playwright } from '../playwright';
 import { SdkObject } from '../../server/instrumentation';
 import { serializeClientSideCallMetadata } from '../../utils';
 import { deviceDescriptors as descriptors }  from '../deviceDescriptors';
-import type { RemoteAddr, RequestContext, ResourceTiming, SecurityDetails } from '../network';
-import { Request, Response, Route } from '../network';
 import { APIRequestContextDispatcher, RequestDispatcher, ResponseDispatcher, RouteDispatcher } from './networkDispatchers';
-import url from 'url';
-import { pipeline } from 'stream/promises';
-import { Transform } from 'stream';
 import type { APIRequestContext } from '../fetch';
 import { GlobalAPIRequestContext } from '../fetch';
-import { TLSSocket } from 'tls';
-import type { AddressInfo } from 'net';
+import { MockingProxy, ServerInterceptionRegistry } from './mockingProxy';
 
 export class LocalUtilsDispatcher extends Dispatcher<SdkObject, channels.LocalUtilsChannel, RootDispatcher> implements channels.LocalUtilsChannel {
   _type_LocalUtils: boolean;
@@ -336,291 +329,9 @@ export class LocalUtilsDispatcher extends Dispatcher<SdkObject, channels.LocalUt
   }
 }
 
-
-type InterceptorResult =
-| { result: 'continue', guid: string, overrides?: NormalizedContinueOverrides }
-| { result: 'abort', guid: string, errorCode: string }
-| { result: 'fulfill', guid: string, response: NormalizedFulfillResponse };
-
-interface EventDelegate {
-  onRequest(request: Request): void;
-  onRequestFinished(request: Request, response: Response): void;
-  onRequestFailed(request: Request): void;
-  onResponse(request: Request, response: Response): void;
-  onRoute(route: Route, request: Request): void;
-}
-
-class ServerInterceptionRegistry extends SdkObject implements RequestContext {
-  private _eventDelegate: EventDelegate;
-  private readonly _requests = new Map<string, Request>(); // TODO: dont memory leak requests
-  fetchRequest: APIRequestContext;
-  private _matches?: (url: string) => boolean;
-
-  constructor(parent: SdkObject, requestContext: APIRequestContext, eventDelegate: EventDelegate) {
-    super(parent, 'serverInterceptionRegistry');
-    this._eventDelegate = eventDelegate;
-    this.fetchRequest = requestContext;
-  }
-
-  setRequestInterceptor(matches?: (url: string) => boolean) {
-    this._matches = matches;
-  }
-
-  handle(url: string, method: string, body: Buffer | null, headers: HeadersArray): Promise<InterceptorResult> {
-    const request = new Request(this, null, null, null, undefined, url, '', method, body, headers);
-    request.setRawRequestHeaders(headers);
-    this._eventDelegate.onRequest(request);
-
-    const guid = request.guid;
-    this._requests.set(guid, request);
-
-    if (!this._matches?.(url))
-      return Promise.resolve({ result: 'continue', guid });
-
-    return new Promise(resolve => {
-      const route = new Route(request, {
-        async abort(errorCode) {
-          resolve({ result: 'abort', guid, errorCode });
-        },
-        async continue(overrides) {
-          resolve({ result: 'continue', guid, overrides });
-        },
-        async fulfill(response) {
-          resolve({ result: 'fulfill', guid, response });
-        },
-      });
-
-      this._eventDelegate.onRoute(route, request);
-    });
-  }
-
-  failed(guid: string, error: string) {
-    const request = this._requests.get(guid);
-    if (!request)
-      throw new Error('Internal error: missing request for response');
-    request._setFailureText(error);
-    this._eventDelegate.onRequestFailed(request);
-  }
-
-  response(guid: string, status: number, statusText: string, headers: HeadersArray, body: () => Promise<Buffer>, httpVersion: string, timing: ResourceTiming, securityDetails: SecurityDetails | undefined, serverAddr: RemoteAddr | undefined) {
-    const request = this._requests.get(guid);
-    if (!request)
-      throw new Error('Internal error: missing request for response');
-    const response = new Response(request, status, statusText, headers, timing, body, false, httpVersion);
-    response.setRawResponseHeaders(headers);
-    response._securityDetailsFinished(securityDetails);
-    response._serverAddrFinished(serverAddr);
-    this._eventDelegate.onResponse(request, response);
-
-    return {
-      finished: async (responseEndTiming: number, transferSize: number, encodedBodySize: number) => {
-        response._requestFinished(responseEndTiming);
-        response.setTransferSize(transferSize);
-        response.setEncodedBodySize(encodedBodySize);
-        response.setResponseHeadersSize(transferSize - encodedBodySize);
-        this._eventDelegate.onRequestFinished(request, response);
-      }
-    };
-  }
-
-  addRouteInFlight(route: Route): void {
-
-  }
-
-  removeRouteInFlight(route: Route): void {
-
-  }
-}
-
-function headersArray(req: Pick<http.IncomingMessage, 'headersDistinct'>): HeadersArray {
-  return Object.entries(req.headersDistinct).flatMap(([name, values = []]) => values.map(value => ({ name, value })));
-}
-
-function headersArrayToOutgoingHeaders(headers: HeadersArray) {
-  const result: http.OutgoingHttpHeaders = {};
-  for (const { name, value } of headers) {
-    if (result[name] === undefined)
-      result[name] = value;
-    else if (Array.isArray(result[name]))
-      result[name].push(value);
-    else
-      result[name] = [result[name] as string, value];
-  }
-  return result;
-}
-
-async function collectBody(req: http.IncomingMessage) {
-  return await new Promise<Buffer>((resolve, reject) => {
-    const chunks: Buffer[] = [];
-    req.on('data', chunk => chunks.push(chunk));
-    req.on('end', () => resolve(Buffer.concat(chunks)));
-    req.on('error', reject);
-  });
-}
-
-class WorkerHttpServer extends HttpServer {
+export class WorkerHttpServer extends HttpServer {
   override _handleCORS(request: http.IncomingMessage, response: http.ServerResponse): boolean {
     return false;
-  }
-}
-
-class MockingProxy {
-  private readonly _registry: ServerInterceptionRegistry;
-
-  constructor(registry: ServerInterceptionRegistry) {
-    this._registry = registry;
-  }
-
-  install(server: WorkerHttpServer) {
-    server.routePrefix('/', (req, res) => {
-      this._proxy(req, res);
-      return true;
-    });
-    server.server().on('connect', (req, socket, head) => {
-      socket.end('HTTP/1.1 405 Method Not Allowed\r\n\r\n');
-    });
-  }
-
-  private async _proxy(req: http.IncomingMessage, res: http.ServerResponse) {
-    if (req.url?.startsWith('/'))
-      req.url = req.url.substring(1);
-
-    // Java URL likes removing double slashes from the pathname.
-    if (req.url?.startsWith('http:/') && !req.url?.startsWith('http://'))
-      req.url = req.url.replace('http:/', 'http://');
-    if (req.url?.startsWith('https:/') && !req.url?.startsWith('https://'))
-      req.url = req.url.replace('https:/', 'https://');
-
-    delete req.headersDistinct.host;
-    const headers = headersArray(req);
-    const body = await collectBody(req);
-    const result = await this._registry.handle(req.url!, req.method!, body, headers);
-    switch (result.result) {
-      case 'abort': {
-        req.destroy(result.errorCode ? new Error(result.errorCode) : undefined);
-        return;
-      }
-      case 'continue': {
-        const { overrides } = result;
-        const proxyUrl = url.parse(overrides?.url ?? req.url!);
-        const httpLib = proxyUrl.protocol === 'https:' ? https : http;
-        const proxyHeaders = overrides?.headers ?? headers;
-        const proxyMethod = overrides?.method ?? req.method;
-        const proxyBody = overrides?.postData ?? body;
-
-        const startAt = monotonicTime();
-        let connectEnd: number | undefined;
-        let connectStart: number | undefined;
-        let dnsLookupAt: number | undefined;
-        let tlsHandshakeAt: number | undefined;
-        let socketBytesReadStart = 0;
-
-        return new Promise<void>(resolve => {
-          const proxyReq = httpLib.request({
-            ...proxyUrl,
-            headers: headersArrayToOutgoingHeaders(proxyHeaders),
-            method: proxyMethod,
-          }, async proxyRes => {
-            const responseStart = monotonicTime();
-            const timings: ResourceTiming = {
-              startTime: startAt / 1000,
-              connectStart: connectStart ? (connectStart - startAt) : -1,
-              connectEnd: connectEnd ? (connectEnd - startAt) : -1,
-              domainLookupStart: -1,
-              domainLookupEnd: dnsLookupAt ? (dnsLookupAt - startAt) : -1,
-              requestStart: -1,
-              responseStart: (responseStart - startAt),
-              secureConnectionStart: tlsHandshakeAt ? (tlsHandshakeAt - startAt) : -1,
-            };
-
-            const socket = proxyRes.socket;
-
-            let securityDetails: SecurityDetails | undefined;
-            if (socket instanceof TLSSocket) {
-              const peerCertificate = socket.getPeerCertificate();
-              securityDetails = {
-                protocol: socket.getProtocol() ?? undefined,
-                subjectName: peerCertificate.subject.CN,
-                validFrom: new Date(peerCertificate.valid_from).getTime() / 1000,
-                validTo: new Date(peerCertificate.valid_to).getTime() / 1000,
-                issuer: peerCertificate.issuer.CN
-              };
-            }
-
-            const address = socket.address() as AddressInfo;
-            const responseBodyPromise = new ManualPromise<Buffer>();
-            const response = this._registry.response(
-                result.guid,
-                proxyRes.statusCode!,
-                proxyRes.statusMessage!, headersArray(proxyRes),
-                () => responseBodyPromise,
-                proxyRes.httpVersion,
-                timings,
-                securityDetails,
-                { ipAddress: address.family === 'IPv6' ? `[${address.address}]` : address.address, port: address.port },
-            );
-
-            try {
-              res.writeHead(proxyRes.statusCode!, proxyRes.headers);
-
-              const chunks: Buffer[] = [];
-              await pipeline(
-                  proxyRes,
-                  new Transform({
-                    transform(chunk, encoding, callback) {
-                      chunks.push(chunk);
-                      callback(undefined, chunk);
-                    },
-                  }),
-                  res
-              );
-              const body = Buffer.concat(chunks);
-              responseBodyPromise.resolve(body);
-
-              response.finished(
-                  monotonicTime() - startAt,
-                  socket.bytesRead - socketBytesReadStart,
-                  body.byteLength
-              );
-              resolve();
-            } catch (error) {
-              this._registry.failed(result.guid, error.toString());
-              resolve();
-            }
-          });
-
-          proxyReq.on('error', error => {
-            this._registry.failed(result.guid, error.toString());
-            res.statusCode = 502;
-            res.end(resolve);
-          });
-          proxyReq.once('socket', socket => {
-            if (proxyReq.reusedSocket)
-              return;
-
-            socketBytesReadStart = socket.bytesRead;
-
-            socket.once('lookup', () => { dnsLookupAt = monotonicTime(); });
-            socket.once('connectionAttempt', () => { connectStart = monotonicTime(); });
-            socket.once('connect', () => { connectEnd = monotonicTime(); });
-            socket.once('secureConnect', () => { tlsHandshakeAt = monotonicTime(); });
-          });
-          proxyReq.end(proxyBody);
-        });
-      }
-      case 'fulfill': {
-        const { response: { status, headers, body, isBase64 } } = result;
-        res.statusCode = status;
-        for (const { name, value } of headers)
-          res.appendHeader(name, value);
-        res.sendDate = false;
-        res.end(Buffer.from(body, isBase64 ? 'base64' : 'utf-8'));
-        return;
-      }
-      default: {
-        throw new Error('Unexpected result');
-      }
-    }
   }
 }
 
